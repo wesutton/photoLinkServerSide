470e0974627e3c1b8d80982b7d69b389f25013fe
willSUtton <wesutton715@gmail.com>
2021-11-23 12:34:46 -0500

    heroku

--- db.js
+++ db.js
@@ -1,6 +1,14 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres'
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // very important
+          }
+      }
+
+
 });
 
 sequelize.authenticate().then(
